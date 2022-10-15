c4d7bb028d80b1eaa179e6b65c23dc6ee040a079
Gabriel Esnack <gabriel_esnack@hotmail.com>
2022-10-15 02:38:35 -0300

    fix: animations when open select

--- src/Select/design-system-select.ts
+++ src/Select/design-system-select.ts
@@ -1,8 +1,7 @@
 import { LitElement, css, html, unsafeCSS, svg } from 'lit'
-
+import {animate, fadeIn, fadeInSlow, fadeOut, flyAbove, flyBelow} from '@lit-labs/motion';
 import { customElement, property, state } from 'lit/decorators.js'
 import {repeat} from 'lit/directives/repeat.js';
-import { ifDefined } from 'lit/directives/if-defined.js';
 import ResetCss from '@unocss/reset/eric-meyer.css'
 import { OptionsType } from './interface';
 import { isItemChecked } from './helper';
@@ -18,11 +17,11 @@ export class DesignSystemSelect extends LitElement {
   static styles = [
     unsafeCSS(ResetCss), 
     css`@unocss-placeholder;`, 
-    // css`
-    //   select {
-    //     width: -webkit-fill-available;
-    //   }
-    // `
+    css`
+      select {
+        width: -webkit-fill-available;
+      }
+    `
   ]
 
   @state()
@@ -69,25 +68,60 @@ export class DesignSystemSelect extends LitElement {
     this.handleMultiOption(item)
   }
 
+  private chevronIcon() {
+    return svg`
+     <svg class="peer-focus:-rotate-180 transition-all transition-duration-500 fill-gray-400 w-3 position-absolute right-3 top-4" viewBox="0 0 512 512">
+      <!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/>
+     </svg>
+    `
+  }
+
+  private checkedIcon() {
+    return svg`
+      <svg class="w-4" viewBox="0 0 512 512">
+      <!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/>
+      </svg>
+    `
+  }
+
   private dropdownItemTemplate(item: OptionsType) {
     const isSelected = this.optionsChecked.length && this.optionsChecked.find(option => isItemChecked(option, item));
     if (!!isSelected)
-      return svg`
+      return html`
       <li class="select-item select-item--selected" @click=${() => this.handleOption(item)}>
-        Option 1
-        <svg class="w-4" viewBox="0 0 512 512">
-          <!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/>
-        </svg>
+        ${item.label}
+        ${this.checkedIcon()}
       </li>
       `
 
     return html`<li class="select-item" @click=${() => this.handleOption(item)}>${item.label}</li>`
   }
 
+  public get sortedOptions() {
+    return this.options.sort((firstOption, secondOption) => {
+      let firstIsChecked = !!this.optionsChecked.find((option) => isItemChecked(option, firstOption))
+      let secondIsChecked = !!this.optionsChecked.find((option) => isItemChecked(option, secondOption))
+   
+      if (firstIsChecked && secondIsChecked ) return 0; // don't worry about sort
+      if (firstIsChecked && !secondIsChecked) return -1; // sort first before second
+   
+       return 1 // sort second after first
+   })
+  }
+
   private dropdownTemplate() {
+    const keyframeOptions: KeyframeAnimationOptions = {
+      duration: 600,
+      fill: 'forwards'
+    }
+
     return html`
-      <ul class="select">
-        ${repeat(this.options, (item) => item.id || item.value, this.dropdownItemTemplate.bind(this) )}
+      <ul class="select" ${animate({
+        keyframeOptions,
+        in: fadeIn,
+        out: fadeOut,
+      })}>
+        ${repeat(this.sortedOptions, (item) => item.id || item.value, this.dropdownItemTemplate.bind(this) )}
       </ul>
     `
   }
@@ -109,7 +143,9 @@ export class DesignSystemSelect extends LitElement {
   render() {
     return html`
       <div class="position-relative">
-        <input type="text" class="input input--normal" placeholder="Select" readonly value=${this.singleValue} @click=${this.handleOpen}></input>
+        <input type="text" class="input input--normal peer" placeholder="Select" readonly value=${this.singleValue} @click=${this.handleOpen} />
+        ${this.chevronIcon()}
+
 
         ${this.shouldRenderDropdown ? this.dropdownTemplate() : null}
       </div>
--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
       ],
       shortcuts: [
         { "input": "h-10 border rounded outline-0 px-4 cursor-pointer text-sm c-gray-700" },
-        { "input--normal": "border-gray-400 focus-visible:b-sky-500 placeholder:c-gray-300" },
+        { "input--normal": "border-gray-400 focus-visible:b-sky-500 placeholder:c-gray-300 " },
         { "input--error": "border-rose-400 focus-visible:b-rose-600" },
         { "select": "border rounded b-gray-300 shadow-md box-shadow mt-2 py-2 position-absolute w-full z-index-50 bg-white" },
         { "select-item": "text-sm c-gray-700 cursor-pointer py-2 px-4 hover:bg-slate-200 " },
