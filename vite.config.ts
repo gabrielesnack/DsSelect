f9491e9a8c0cb97c339467da3372e805f12bf205
Gabriel Esnack <gabriel_esnack@hotmail.com>
2022-10-15 02:56:57 -0300

    fix: border color

--- vite.config.ts
+++ vite.config.ts
@@ -20,8 +20,8 @@ export default defineConfig({
         presetMini()
       ],
       shortcuts: [
-        { "input": "h-10 border rounded outline-0 px-4 cursor-pointer text-sm c-gray-700" },
-        { "input--normal": "border-gray-400 focus-visible:b-sky-500 placeholder:c-gray-300 " },
+        { "input": "h-10 border rounded shadow-md outline-0 px-4 cursor-pointer text-sm c-gray-700" },
+        { "input--normal": "b-gray-300 focus-visible:b-sky-500 placeholder:c-gray-300 " },
         { "input--error": "border-rose-400 focus-visible:b-rose-600" },
         { "select": "border rounded b-gray-300 shadow-md box-shadow mt-2 py-2 position-absolute w-full z-index-50 bg-white" },
         { "select-item": "text-sm c-gray-700 cursor-pointer py-2 px-4 hover:bg-slate-200 " },
