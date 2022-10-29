fb82eaa16b1d9f6aff6f10dcac35cde156956bb1
Gabriel Esnack <gabriel_esnack@hotmail.com>
2022-10-29 17:48:26 -0300

    fix: methods declaration

--- src/Select/ds-select.ts
+++ src/Select/ds-select.ts
@@ -1,82 +1,95 @@
-import { LitElement, css, html, unsafeCSS, svg, PropertyValueMap } from 'lit'
-import {animate, fadeIn, fadeInSlow, fadeOut, flyAbove, flyBelow} from '@lit-labs/motion';
-import { customElement, property, state } from 'lit/decorators.js'
-import { cache } from 'lit/directives/cache.js';
-import {repeat} from 'lit/directives/repeat.js';
-import {ifDefined} from 'lit/directives/if-defined.js';
-import ResetCss from '@unocss/reset/eric-meyer.css'
-import { HTMLElementEvent, OptionsType } from './interface';
-import { filterByLabel, isChecked, isOptionChecked, preventPropagation, sliceLabel, sortByItemsChecked } from './helper';
-
+import { LitElement, css, html, unsafeCSS, svg, PropertyValueMap } from "lit";
+import {
+  animate,
+  fadeIn,
+  fadeInSlow,
+  fadeOut,
+  flyAbove,
+  flyBelow,
+} from "@lit-labs/motion";
+import { customElement, property, state } from "lit/decorators.js";
+import { cache } from "lit/directives/cache.js";
+import { repeat } from "lit/directives/repeat.js";
+import { ifDefined } from "lit/directives/if-defined.js";
+import ResetCss from "@unocss/reset/eric-meyer.css";
+import { HTMLElementEvent, OptionsType } from "./interface";
+import {
+  filterByLabel,
+  isChecked,
+  isOptionChecked,
+  sortByItemsChecked,
+} from "./helper";
 
 const chevronIconTemplate = cache(svg`
 <svg class="peer-focus:-rotate-180 transition-all transition-duration-200 fill-gray-400 w-3 position-absolute right-3 top-4" viewBox="0 0 512 512">
  <!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/>
 </svg>
-`)
+`);
 
-@customElement('ds-select')
+@customElement("ds-select")
 export class DsSelect extends LitElement {
   static styles = [
-    unsafeCSS(ResetCss), 
-    css`@unocss-placeholder;`, 
+    unsafeCSS(ResetCss),
+    css`
+      @unocss-placeholder;
+    `,
     css`
       select {
         width: -webkit-fill-available;
       }
-    `
-  ]
+    `,
+  ];
 
   constructor() {
-    super()
+    super();
     this._computedOptions = this.options;
   }
 
   @property()
-  placeholder: string = 'Escolha uma fruta'
+  placeholder: string = "Escolha uma fruta";
 
-  @property({type: Boolean, attribute: 'is-multi'})
+  @property({ type: Boolean, attribute: "is-multi" })
   isMulti = false;
 
-  @property({ type: Boolean, attribute: 'is-filterable'})
+  @property({ type: Boolean, attribute: "is-filterable" })
   isFilterable: true | undefined = undefined;
 
   @property()
   options = [
-    { id: 1, value: '1', label: 'Banana com cauda de chocolate'},
-    { id: 2, value: '2', label: 'Ana'},
-    { id: 3, value: '3', label: 'Maça'},
-    { id: 4, value: '4', label: 'Laranja'},
-  ] as OptionsType[]
+    { id: 1, value: "1", label: "Banana com cauda de chocolate" },
+    { id: 2, value: "2", label: "Ana" },
+    { id: 3, value: "3", label: "Maça" },
+    { id: 4, value: "4", label: "Laranja" },
+  ] as OptionsType[];
 
   @state()
   private _isOpen = false;
-
   private get isOpen() {
-    return this._isOpen
+    return this._isOpen;
   }
-
   private set isOpen(value: boolean) {
-    this.shouldHideValueOnFiltering(value);
+    this._shouldHideValueOnFiltering(value);
     this._isOpen = value;
   }
 
   @state()
-  private optionsChecked = [] as OptionsType[]
+  private optionsChecked = [] as OptionsType[];
 
   @state()
-  private value : string | null | number = null;
+  private value: string | null | number = null;
 
   @state()
-  private _computedOptions: OptionsType[] = []
-  private get computedOptions() { return this._computedOptions; }
+  private _computedOptions: OptionsType[] = [];
+  private get computedOptions() {
+    return this._computedOptions;
+  }
   private set computedOptions(value) {
-    const sortedValue = sortByItemsChecked(value, this.optionsChecked)
+    const sortedValue = sortByItemsChecked(value, this.optionsChecked);
     this._computedOptions = sortedValue;
   }
 
   // events
-  private _onChange(evt: HTMLElementEvent<HTMLInputElement> ) {
+  private _onChange(evt: HTMLElementEvent<HTMLInputElement>) {
     if (this.isMulti) return;
 
     if (this.optionsChecked.length) {
@@ -85,137 +98,157 @@ export class DsSelect extends LitElement {
 
       return;
     }
-    
-    evt.target.value = '';
+
+    evt.target.value = "";
   }
 
   private _onInput(evt: HTMLElementEvent<HTMLInputElement>) {
     // define debounce
     const value = evt.target.value;
-    this.computedOptions = filterByLabel(value, this.options)
+    this.computedOptions = filterByLabel(value, this.options);
   }
 
-  private _onClick(e: Event) {
+  private _onClick() {
     this.isOpen = !this.isOpen;
   }
 
-  private handleSingleOption(item: OptionsType) {
+  private _onRemoveTag() {
+    this.optionsChecked.shift();
+    this.requestUpdate("optionsChecked");
+  }
+
+  private _handleDropdownPlacement() {
+    console.log("changed");
+  }
+
+  private _handleSingleOption(item: OptionsType) {
     this.isOpen = false;
-    this.optionsChecked = [item]
+    this.optionsChecked = [item];
     this.value = item.label.toString();
   }
 
-  private handleMultiOption(item: OptionsType) {
-    const foundIndex = this.optionsChecked.findIndex(option => isChecked(option, item))
+  private _handleMultipleOption(item: OptionsType) {
+    const foundIndex = this.optionsChecked.findIndex((option) =>
+      isChecked(option, item)
+    );
 
     if (foundIndex !== -1) {
-      this.optionsChecked = this.optionsChecked.filter((_, idx) => idx !== foundIndex)
+      this.optionsChecked = this.optionsChecked.filter(
+        (_, idx) => idx !== foundIndex
+      );
       return;
     }
 
     this.optionsChecked = [...this.optionsChecked, item];
   }
 
-  private selectOption(item: OptionsType) : void {
-    if (!this.isMulti) return this.handleSingleOption(item);
+  private _handleOption(item: OptionsType): void {
+    if (!this.isMulti) return this._handleSingleOption(item);
 
-    this.handleMultiOption(item)
+    this._handleMultipleOption(item);
   }
 
-  private _onRemoveTag() {
-    this.optionsChecked.shift()
-    this.requestUpdate('optionsChecked')
-  }
-
-  private get shouldRenderPlaceholder() {
+  private get _shouldRenderPlaceholder() {
     const hasOptionsSelected = this.optionsChecked.length;
 
-    if (this.isMulti && hasOptionsSelected) return ''
+    if (this.isMulti && hasOptionsSelected) return "";
 
-    if (!this.isMulti && hasOptionsSelected) return this.placeholder
+    if (!this.isMulti && hasOptionsSelected) return this.placeholder;
 
-    return this.placeholder
+    return this.placeholder;
   }
 
-  private shouldHideValueOnFiltering(isDropdownOpened: boolean) {
+  private _shouldHideValueOnFiltering(isDropdownOpened: boolean) {
     if (this.isMulti) return;
 
     if (isDropdownOpened) {
-      this.value = '';
+      this.value = "";
       this.computedOptions = this.options;
       return;
     }
-    
-    this.value = this.optionsChecked[0]?.label
+
+    this.value = this.optionsChecked[0]?.label || "";
   }
 
-  private get shouldRenderDropdown() {
+  private get _shouldRenderDropdown() {
     return this.isOpen && !!this.options.length;
   }
 
-  private get renderTagsTemplate() {
+  private get _renderTagsTemplate() {
     if (!this.isMulti) return;
     if (!this.optionsChecked.length) return;
-    // if (this.isOpen && this.isFilterable) return;
 
-    const hasMoreThanOne = this.optionsChecked.length > 1
+    const hasMoreThanOne = this.optionsChecked.length > 1;
 
     return html`
-      <ds-tag 
-        is-closable 
-        is-truncate 
+      <ds-tag
+        is-closable
+        is-truncate
         class="max-w-2/4 shrink"
-        @dsTagRemove=${this._onRemoveTag} 
+        @dsTagRemove=${this._onRemoveTag}
       >
         ${this.optionsChecked[0].label}
       </ds-tag>
-      ${hasMoreThanOne ? html`<ds-tag>+${this.optionsChecked.length - 1}</ds-tag>`: ''}
-    `
+      ${hasMoreThanOne
+        ? html`<ds-tag>+${this.optionsChecked.length - 1}</ds-tag>`
+        : ""}
+    `;
   }
 
-  private get renderOptionsTemplate() {
-    return repeat(this.computedOptions, (item) => item?.id || item?.value, (option) => {
-      const isSelected = isOptionChecked(option, this.optionsChecked) || undefined 
+  private get _renderOptionsTemplate() {
+    return repeat(
+      this.computedOptions,
+      (item) => item?.id || item?.value,
+      (option) => {
+        const isSelected =
+          isOptionChecked(option, this.optionsChecked) || undefined;
 
-      return html`
-        <ds-select-item 
-          .isSelected="${isSelected}"
-          @dsSelectItemClick=${() => this.selectOption(option)}
-        >
-          ${option?.label}
-        </ds-select-item>
-    `})
+        return html`
+          <ds-select-item
+            .isSelected="${isSelected}"
+            @dsSelectItemClick=${() => this._handleOption(option)}
+          >
+            ${option?.label}
+          </ds-select-item>
+        `;
+      }
+    );
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    window.addEventListener("scroll", this._handleDropdownPlacement);
+  }
+  disconnectedCallback() {
+    window.removeEventListener("scroll", this._handleDropdownPlacement);
+    super.disconnectedCallback();
   }
 
   render() {
     return html`
-      <div 
-        class="select select--normal"
-        @click=${this._onClick} 
-      >
-        ${this.renderTagsTemplate}
+      <div class="select select--normal" @click=${this._onClick}>
+        ${this._renderTagsTemplate}
 
-        <input 
-          type="text" 
-          class="input-inner w-full"
-          placeholder=${ifDefined(this.shouldRenderPlaceholder)} 
-          readonly=${ifDefined(!this.isFilterable || undefined)} 
+        <input
+          type="text"
+          class="input-inner w-full cursor-pointer"
+          placeholder=${ifDefined(this._shouldRenderPlaceholder)}
+          readonly=${ifDefined(!this.isFilterable || undefined)}
           .value="${this.value}"
           @input=${this._onInput}
           @change=${this._onChange}
         />
 
-        <ds-menu .isOpen=${this.shouldRenderDropdown}>
-          ${this.renderOptionsTemplate}
+        <ds-menu .isOpen=${this._shouldRenderDropdown}>
+          ${this._renderOptionsTemplate}
         </ds-menu>
       </div>
-    `
+    `;
   }
-
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    'ds-select': DsSelect
+    "ds-select": DsSelect;
   }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -21,10 +21,10 @@ export default defineConfig({
       ],
       shortcuts: [
         { "tag": "flex items-center gap-2 border rounded py-1 px-2 bg-gray-100 border-color-gray-200 c-gray-500 font-light text-xs" },
-        { "input-inner": "border-none outline-0 text-sm c-gray-700" },
+        { "input-inner": "border-none outline-0 text-sm c-gray-700 pointer-events-auto" },
         { "select": "position-relative flex items-center justify-start gap-2 h-10 border rounded shadow-md outline-0 px-4 cursor-pointer text-sm c-gray-700 placeholder:c-gray-300" },
-        { "select--normal": "border-gray-300 focus-visible:b-sky-500" },
-        { "select--error": "border-rose-400 focus-visible:b-rose-600" },
+        { "select--normal": "border-gray-300 focus-within:b-sky-500" },
+        { "select--error": "border-rose-400 focus-within:b-rose-600" },
         { "menu": "position-absolute top-full left-0 w-full z-index-50 border rounded b-gray-300 shadow-md box-shadow mt-2 py-2 bg-white" },
         { "select-item": "text-sm c-gray-700 cursor-pointer py-2 px-4 hover:bg-slate-200 " },
         { "select-item--selected": "flex justify-between c-sky-500 hover:c-sky-500 fill-sky-500 font-bold" }
